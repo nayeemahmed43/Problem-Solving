700edd078b4a644a5f9b71b5ce10e64c12f67168
nayeemahmed43 <36023423+nayeemahmed43@users.noreply.github.com>
2020-01-02 23:18:25 +0600

    Add files via upload

---
+++ 1.primeCheck.c
@@ -0,0 +1,23 @@
+//write a program that determines if a number is prime or not
+#include <stdio.h>
+int main(){
+    int n, i, flag=0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for(i=2; i<=n/2; i++){ //condition for non prime
+        if(n%i == 0){
+            flag = 1;
+            break;
+        }
+    }
+    if(n == 1){
+        printf("1 is neither a prime nor a composite");
+    }else{
+        if(flag == 0)
+            printf("%d is a prime number",n);
+        else
+            printf("%d is not a prime number",n);
+    }
+    return 0;
+}
